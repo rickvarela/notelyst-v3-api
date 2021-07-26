35ca5f8bd97e88ff03800879efedf18da8cba29f
Rick Varela <rickvarela91@gmail.com>
2021-07-26 09:26:36 -0700

    Update cors settings

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 
 app.get('/test', (req, res) => {
   res.send('test').status(400);
